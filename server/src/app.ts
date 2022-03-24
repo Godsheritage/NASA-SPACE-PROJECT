2bd674dcc3720395962e95c22112b00ffa5933d3
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-24 18:46:57 +0100

    worked on launches model

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, "..", "public ")));
 
 app.use(express.json());
 app.use(planetRouter)
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public ", "index.html"));
 });
