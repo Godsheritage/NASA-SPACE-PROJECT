d1fb9c1167a7d9590a5282878bb4bc52711c8c82
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-24 18:17:42 +0100

    updates

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,7 @@ app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname, "..", "public ")));
 
 app.use(express.json());
-app.use(planetRouter);
+app.use(planetRouter)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public ", "index.html"));
 });
