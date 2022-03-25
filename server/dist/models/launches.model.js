ea357a3e8543034449c365192f89dedf0e88b2d3
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-25 11:55:54 +0100

    updates

--- server/dist/models/launches.model.js
+++ server/dist/models/launches.model.js
@@ -1,18 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const launches = new Map;
+const launches = new Map();
+let latestFlightNumber = 100;
 const launch = {
     flightNumber: 100,
-    mission: 'Kepler Exploration x',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    destination: 'kepler-442 b',
-    customers: ['Nasa', 'Godsheritage'],
+    mission: "Kepler Exploration x",
+    rocket: "Explorer IS1",
+    launchDate: new Date("December 27, 2030"),
+    destination: "kepler-442 b",
+    customers: ["Nasa", "Godsheritage"],
     upcoming: true,
     success: true,
 };
 launches.set(launch.flightNumber, launch);
 const getAllLaunches = () => {
     return Array.from(launches.values());
+};
+const addNewLaunch = (launch) => {
+    latestFlightNumber++;
+    launches.set(latestFlightNumber, Object.assign(launch, {
+        upcoming: true,
+        success: true,
+        flightNumber: latestFlightNumber,
+    }));
 };
 exports.default = getAllLaunches;
--- server/src/models/launches.model.ts
+++ server/src/models/launches.model.ts
@@ -1,20 +1,34 @@
-const launches = new Map
+const launches = new Map();
+
+let latestFlightNumber = 100;
 
 const launch = {
-    flightNumber:100,
-    mission:'Kepler Exploration x',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    destination:'kepler-442 b',
-    customers: ['Nasa', 'Godsheritage'],
-    upcoming:true,
-    success:true,
-}
+  flightNumber: 100,
+  mission: "Kepler Exploration x",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "kepler-442 b",
+  customers: ["Nasa", "Godsheritage"],
+  upcoming: true,
+  success: true,
+};
 
-launches.set(launch.flightNumber, launch)
+launches.set(launch.flightNumber, launch);
 
 const getAllLaunches = () => {
-    return Array.from(launches.values())
-}
+  return Array.from(launches.values());
+};
 
-export default getAllLaunches
+const addNewLaunch = (launch: any) => {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+        upcoming:true,
+        success: true, 
+      flightNumber: latestFlightNumber,
+    })
+  );
+};
+
+export default getAllLaunches;
