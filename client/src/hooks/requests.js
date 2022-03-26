ec05b26d9b5fa039c2f67beece95029a05559ffc
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-26 12:36:01 +0100

    working on the delete api function

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -20,12 +20,25 @@ const httpGetLaunches = async () => {
 // Submit given launch data to launch system.
 
 const httpSubmitLaunch = async (launch) => {
-  return await axios.post(`${API_URL}/launches`, launch);
+  try {
+    await axios.post(`${API_URL}/launches`, launch);
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 };
 
-async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
-}
+// Delete launch with given ID.
+const httpAbortLaunch = async (id) => {
+  try {
+    await axios.delete(`${API_URL}/launches/${id}`);
+  } catch (err) {
+    console.log(err)
+    return {
+      ok: false,
+    };
+  }
+};
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
