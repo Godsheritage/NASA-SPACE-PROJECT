05555ce13a7edaeb31180458b057e5f8260bad6c
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-31 15:45:01 +0100

    updates

--- server/src/routes/launches/launches.mongo.ts
+++ server/src/routes/launches/launches.mongo.ts
@@ -1,1 +1,30 @@
-import mongoose from "mongoose";
+// to create the schema
+
+import mongoose, { model, Schema, Model, Document } from "mongoose";
+// where the schema will be stored
+
+interface IUser extends Document {
+  flightNumber: string;
+  launchDate: Date;
+  mission: string;
+  rocket: string;
+  target: string;
+  customers: string[];
+  upcoming: Boolean;
+  sucess: Boolean;
+}
+
+const UserSchema: Schema = new mongoose.Schema({
+  flightNumber: { type: Date, required: true },
+  launchDate: { type: String, required: true },
+  mission: { type: String, required: true },
+  rocket: { type: String, required: true },
+  target: { type: String, required: true },
+  customers: [String],
+  upcoming: { type: Boolean, required: true },
+  sucess: { type: Boolean, default:true, required: true },
+});
+
+// const launchesSchema:Schema = new mongoose.Schema<types>({
+//   flightNumber: {type:string, required: true}
+// })
