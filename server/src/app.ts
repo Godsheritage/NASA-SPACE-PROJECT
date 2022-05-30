294da26c6c884dd6aab5b568d6ee68265f10f354
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-30 16:32:54 +0100

    working on code updates

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,24 @@
 import cors from "cors";
 import path from "path";
 import morgan from "morgan";
+import dotenv from 'dotenv'
 import express from "express";
 import planetRouter from "./routes/planets/planets.routers";
 import launchesRouter from "./routes/launches/launches.routers";
 
 const app = express();
-
+dotenv.config()
 app.use(cors());
 
 app.use(morgan("combined"));
 app.use(express.json());
-app.use('/planets', planetRouter);
-app.use('/launches', launchesRouter);
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.get("/*", (req , res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
+app.use("/planets", planetRouter);
+app.use("/launches", launchesRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "public")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  });
+}
 
 export default app;
